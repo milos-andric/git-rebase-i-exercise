0baef33e7fd155387379e41a70da99b98cb5a897
milos-andric <55629183+milos-andric@users.noreply.github.com>
2022-09-29 21:32:20 +0200

    initial commit w/ instruction

---
+++ main.cpp
@@ -0,0 +1,7 @@
+int main(int argc, char* argv[])
+{
+	// make a program that takes a string as a parameter
+	// first reverse it
+	// delete all all leading and trailing whitespaces from the string
+	// and finally prints it first the right way then in reverse on the same line
+}
