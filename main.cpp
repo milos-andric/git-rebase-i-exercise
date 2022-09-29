dface36c767eee71ec76be592c8a965c1474aa65
milos-andric <55629183+milos-andric@users.noreply.github.com>
2022-09-29 23:55:50 +0200

    Philip assigned

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 int main(int argc, char* argv[])
 {
 	// make a program that takes a string as a parameter
-	// first reverse it
+	// first reverse it - Assigned to Philip
 	// delete all all leading and trailing whitespaces from the string
 	// and finally prints it first the right way then in reverse on the same line
+
+	if (argc == 1){
+		std::cout << "./a.out <your string here>" << std::endl;
+		std::cout << "please specify string to process" << std::endl;
+		return (1);
+	}
+	else if (argc != 2){
+		return (1);
+	}
+
 }
